bd44cd7320dc1f246022e6a9fae085b61b48dd00
bedoya1207 <mbedoyao@est.ups.edu.ec>
2021-05-23 14:27:24 -0500

    loginphpcompletoypruebas

---
+++ Public/Controladores/Login.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include '../../Config/conexionBD.php';
+$usuario = isset($_POST["correo"]) ? trim($_POST["correo"]): null;
+$contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
+$sql = "SELECT * FROM  usuario WHERE usu_correo = '$usuario' and usu_password MD5('$contrasena')";
+$result = $conn->query($sql);
+if ($result-> num_rows> 0){
+    $_SESSION['isLogged'] = TRUE;
+    echo "<p>Se ha creado los datos personales correctamemte!!!</p>";
+
+} else {
+   header("Location: ../vista/login.html");
+   echo"<p> usuario o contraseña equivocados</p>";
+}
+$conn -> close();
+echo "<a href='../vista/Login.html'>Regresar</a>";
+?>
