78025e39aa703287b38d29ae6408ccd5f5b108a4
bedoya1207 <mbedoyao@est.ups.edu.ec>
2021-05-24 11:50:14 -0500

    cerrarsesionphp

--- Admin/Vista/Admin/MenuAdmin.php
+++ Admin/Vista/Admin/MenuAdmin.php
@@ -3,8 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Administrador</title>
+    <link rel="stylesheet" type="text/css" href="../Controladores/CSS/reglas.css"/>
+    
+    <link rel="stylesheet" type="text/css" href="../Controladores/CSS/index.css"/>
+    
+    <link rel="stylesheet" type="text/css" href="../Controladores/CSS/2-columnas.css"/>
 </head>
-<body>
+<body style="background-color:#094f77;">
+<header>
+<img src="../../../imagenes/banner.jpg" alt="">
+<h1 style="color: azure;" >Guia telefonica</h1>
+</header>
+
+
+
+
+
   
 </body>
 </html>
