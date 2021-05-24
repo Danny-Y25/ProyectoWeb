317e40b9d2197f4192d5bf7f19cd300e6a373cf5
Danny-Y25 <dyungay@est.ups.edu.ec>
2021-05-24 12:02:17 -0500

    Menu Usuariov1

--- Admin/Vista/Usuario/MenuUser.php
+++ Admin/Vista/Usuario/MenuUser.php
@@ -36,9 +36,12 @@
  header("Location: /ProyectoWeb/public/VIsta/Login.html");
  }
  include '../../../Config/conexionBD.php';
- 
+ $cuenta = $_GET['correo'];
+ echo($cuenta);
  $sql = "SELECT * FROM usuario";
- $result = $conn->query($sql);
+ $sql4 = "SELECT * FROM usuario u, telefono t WHERE u.usu_cedula=t.usu_cedula and u.usu_eliminado = 'N' and u.usu_correo='$cuenta'";
+
+ $result = $conn->query($sql4);
 
  if ($result->num_rows > 0) {
 
@@ -47,10 +50,10 @@
     echo " <td>" . $row["usu_cedula"] . "</td>";
     echo " <td>" . $row['usu_nombres'] ."</td>";
     echo " <td>" . $row['usu_apellidos'] . "</td>";
-    echo " <td>" . $row['usu_direccion'] . "</td>";
-    echo " <td>" . $row['usu_telefono'] . "</td>";
     echo " <td>" . $row['usu_correo'] . "</td>";
-    echo " <td>" . $row['usu_fecha_nacimiento'] . "</td>";
+    echo " <td>" . $row['tel_numero'] . "</td>";
+    echo " <td>" . $row['tel_tipo'] . "</td>";
+    echo " <td>" . $row['tel_operadora'] . "</td>";
     echo " <td> <a href='eliminar.php?codigo=" . $row['usu_codigo'] . "'>Eliminar</a> </td>";
     echo " <td> <a href='modificar.php?codigo=" . $row['usu_codigo'] . "'>Modificar</a> </td>";
     echo " <td> <a href='cambiar_contrasena.php?codigo=" . $row['usu_codigo'] . "'>Cambiar
@@ -63,7 +66,7 @@
  echo " <td colspan='7'> No existen usuarios registradas en el sistema </td>";
  echo "</tr>";
  }
- echo " <td> <a href='../../../config/cerrar_sesion.php'>Cerrar Sesion</a> </td>";
+ echo " <td> <a href='../../../config/Cerrarsesion.php'>Cerrar Sesion</a> </td>";
  $conn->close();
  ?>
  </table>
