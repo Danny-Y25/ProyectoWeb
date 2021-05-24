bf1dd394ae8cb45d44748954c32a2325e66c6490
bedoya1207 <mbedoyao@est.ups.edu.ec>
2021-05-24 13:56:37 -0500

    metodosdecontroladmin

--- Admin/Vista/Admin/MenuAdmin.php
+++ Admin/Vista/Admin/MenuAdmin.php
@@ -32,12 +32,10 @@
   header("Location: /SistemaDeGestion/public/vista/login.html");
   }
  include '../../../config/conexionBD.php';
- $sql = "SELECT * FROM usuario";
+ $sql = "SELECT * FROM usuario u, telefono t WHERE u.usu_cedula=t.usu_cedula and u.usu_eliminado = 'N' and u.usu_correo=t.usu_correo";
  $result = $conn->query($sql);
 
- if ($result->num_rows > 0) {
-  
-        
+ if ($result->num_rows > 0) {       
             while($row = $result->fetch_assoc()) {
                
                     echo "<tr>";
@@ -45,10 +43,14 @@
                     echo " <td>" . $row['usu_nombres'] ."</td>";
                     echo " <td>" . $row['usu_apellidos'] . "</td>";
                     echo " <td>" . $row['usu_correo'] . "</td>";
+                    echo " <td>" . $row['tel_tipo'] . "</td>";
+                    echo " <td>" . $row['tel_operadora'] . "</td>";
+                    echo " <td>" . $row['tel_numero'] . "</td>";
                     echo " <td> <a href='eliminar.php?codigo=" . $row['usu_codigo'] . "'>Eliminar</a> </td>";
                     echo " <td> <a href='modificar.php?codigo=" . $row['usu_codigo'] . "'>Modificar</a> </td>";
                     echo " <td> <a href='cambiar_contrasena.php?codigo=" . $row['usu_codigo'] . "'>Cambiar
                    contraseña</a> </td>";
+                   echo " <td> <a href='../../../Public/Controladores/Agregar.php?codigo=" . $row['usu_codigo'] . "'>Agregar usuario</a> </td>";
                     echo "</tr>";
                 
                
