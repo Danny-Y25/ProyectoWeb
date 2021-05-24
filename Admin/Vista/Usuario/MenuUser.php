a288dd542665fd58b5efcb70e3bdca5f7cec585c
Danny-Y25 <dyungay@est.ups.edu.ec>
2021-05-24 16:40:15 -0500

    Commit alteracion de MenuUser

--- Admin/Vista/Usuario/MenuUser.php
+++ Admin/Vista/Usuario/MenuUser.php
@@ -22,16 +22,14 @@
     <div id="informacion"><b>Datos de la persona</b></div>
 
 
-
- <table style="width:100%">
+ <h1>Informacion Del Usuario</h1>
+ <table style="width:70%; border: black 1px solid " >
  <tr>
- <th>Cedula</th>
- <th>Nombres</th>
- <th>Apellidos</th>
- <th>Dirección</th>
- <th>Telefono</th>
- <th>Correo</th>
- <th>Fecha Nacimiento</th>
+ <th style="text-align: left">Cedula</th>
+ <th style="text-align: left">Nombres</th>
+ <th style="text-align: left">Apellidos</th>
+ <th style="text-align: left">Correo</th>
+
  </tr>
  <?php
  session_start();
@@ -41,8 +39,8 @@
  include '../../../Config/conexionBD.php';
  $cuenta = $_GET['correo'];
  //echo($cuenta);
- $sql = "SELECT * FROM usuario";
- $sql4 = "SELECT * FROM usuario u, telefono t WHERE u.usu_cedula=t.usu_cedula and u.usu_correo='$cuenta'";
+
+ $sql4 = "SELECT * FROM usuario WHERE usu_correo='$cuenta'";
 
  $result = $conn->query($sql4);
 
@@ -54,15 +52,10 @@
     echo " <td>" . $row['usu_nombres'] ."</td>";
     echo " <td>" . $row['usu_apellidos'] . "</td>";
     echo " <td>" . $row['usu_correo'] . "</td>";
-    echo " <td>" . $row['tel_numero'] . "</td>";
-    echo " <td>" . $row['tel_tipo'] . "</td>";
-    echo " <td>" . $row['tel_operadora'] . "</td>";
     echo " <td> <a href='Eliminar.php?codigo=" . $row['usu_codigo'] . "'>Eliminar</a> </td>";
     echo " <td> <a href='Modificar.php?codigo=" . $row['usu_codigo'] . "'>Modificar</a> </td>";
     echo " <td> <a href='Cambiar_contrasena.php?codigo=" . $row['usu_codigo'] . "'>Cambiarcontraseña</a> </td>";
-    echo " <td> <a href='Eliminar.php?codigo=" . $row['usu_codigo'] . "'>Eliminar_Telefono</a> </td>";
-    echo " <td> <a href='Modificar.php?codigo=" . $row['usu_codigo'] . "'>Modificar_Telefono</a> </td>";
-    echo " <td> <a href='Cambiar_contrasena.php?codigo=" . $row['usu_codigo'] . "'>Agregar_telefono</a> </td>";
+   
     
     echo "</tr>";
  }
@@ -72,10 +65,55 @@
  echo " <td colspan='7'> No existen usuarios registradas en el sistema </td>";
  echo "</tr>";
  }
- echo " <td> <a href='../../../config/Cerrarsesion.php'>Cerrar Sesion</a> </td>";
+ //echo " <td> <a href='../../../config/Cerrarsesion.php'>Cerrar_Sesion</a> </td>";
  
  $conn->close();
  ?>
+ 
  </table>
+
+ <h1>Informacion Telefonica</h1>
+
+ <table style="width:70%; border: black 1px solid">
+ <tr>
+ <th style="text-align: left">Telefono</th>
+ <th style="text-align: left">Tipo</th>
+ <th style="text-align: left">Operadora</th>
+ </tr>
+ <?php
+ include '../../../Config/conexionBD.php';
+ $cuenta = $_GET['correo'];
+ //echo($cuenta);
+ $sql = "SELECT * FROM usuario u, telefono t WHERE u.usu_cedula=t.usu_cedula and u.usu_correo='$cuenta'";
+ $sql4 = "SELECT * FROM usuario u, telefono t WHERE u.usu_cedula=t.usu_cedula and u.usu_correo='$cuenta'";
+
+ $result = $conn->query($sql4);
+
+ if ($result->num_rows > 0) {
+
+ while($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo " <td>" . $row['tel_numero'] . "</td>";
+    echo " <td>" . $row['tel_tipo'] . "</td>";
+    echo " <td>" . $row['tel_operadora'] . "</td>";
+    echo " <td> <a href='Eliminar.php?codigo=" . $row['usu_codigo'] . "'>Eliminar</a> </td>";
+    echo " <td> <a href='Modificar.php?codigo=" . $row['usu_codigo'] . "'>Modificar</a> </td>";
+    echo " <td> <a href='Cambiar_contrasena.php?codigo=" . $row['usu_codigo'] . "'>Agregar</a> </td>";
+    echo "</tr>";
+ }
+ 
+ } else {
+ echo "<tr>";
+ echo " <td colspan='7'> No existen usuarios registradas en el sistema </td>";
+ echo "</tr>";
+ }
+ 
+ 
+ $conn->close();
+ ?>
+ 
+ </table>
+ <br>
+<button><a href='../../../config/Cerrarsesion.php'>Cerrar Sesion</a></button>
 </body>
 </html>
